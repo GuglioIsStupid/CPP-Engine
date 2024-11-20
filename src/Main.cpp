7f4a3da7db529556aea057d452c145af1231fe3c
GuglioIsStupid <bradsbluetab@gmail.com>
2024-11-20 12:19:00 -0500

    actually put the { somewhere else in Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,10 +2,11 @@
 #include "Engine/Engine.hpp"
 
 #ifdef _WIN32
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int) {
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int)
 #else
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
 #endif
+{
     Engine::Engine engine(1280, 720, "Engine");
 
     engine.Run();
